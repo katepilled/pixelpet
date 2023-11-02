e8f7e9ab6320643c2703250c98920387c4a60adf
katepilled <katehyoon@gmail.com>
2023-11-01 23:47:11 -0400

    comment out code

--- db.mjs
+++ db.mjs
@@ -1,22 +1,22 @@
-import mongoose from 'mongoose';
-const { Schema } = mongoose;
+// import mongoose from 'mongoose';
+// const { Schema } = mongoose;
 
-const userSchema = new mongoose.Schema({
-    username: { type: String, required: true },
-    hash: { type: String, required: true },
-    pet: { type: mongoose.Schema.Types.ObjectId, ref: 'Pet' },
-  });
+// const userSchema = new mongoose.Schema({
+//     username: { type: String, required: true },
+//     hash: { type: String, required: true },
+//     pet: { type: mongoose.Schema.Types.ObjectId, ref: 'Pet' },
+//   });
 
-  const mongoose = require('mongoose');
+//   const mongoose = require('mongoose');
 
-const petSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  age: { type: Number, required: true },
-  happiness_level: { type: Number},
-  hunger_level: { type: Number},
-  cleanliness: { type: Number},
-});
+// const petSchema = new mongoose.Schema({
+//   name: { type: String, required: true },
+//   owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+//   age: { type: Number, required: true },
+//   happiness_level: { type: Number},
+//   hunger_level: { type: Number},
+//   cleanliness: { type: Number},
+// });
 
-const User = mongoose.model('User', userSchema);
-const Pet = mongoose.model('Pet', petSchema);
+// const User = mongoose.model('User', userSchema);
+// const Pet = mongoose.model('Pet', petSchema);
