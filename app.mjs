84a2679d1d9fbe96633c9b9449faafb8ecffad19
katepilled <katehyoon@gmail.com>
2023-11-09 20:34:01 -0500

    init

--- app.mjs
+++ app.mjs
@@ -6,4 +6,22 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// configure templating to hbs
+app.set("view engine", "hbs");
+
+// will update later to landing page
+// initial form for milestone 2
+app.get("/", (req, res) => {
+    res.render("naming");
+})
+
+app.get("/name", (req, res) => {
+    res.render("naming");
+})
+
+app.post("/name", (req, res) => {
+    res.render("naming")
+
+})
+
 app.listen(process.env.PORT || 3000);
