9ef045d7dc87c335e1055b9114c6f241d2c77b5d
katepilled <katehyoon@gmail.com>
2023-12-04 02:24:59 -0500

    move files

--- src/models/pet.mjs
+++ src/models/pet.mjs
@@ -1,14 +1,15 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const PetSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  age: { type: Number, required: true },
-  happiness_level: { type: Number },
-  hunger_level: { type: Number },
-  cleanliness: { type: Number },
+	name: { type: String, required: true, default: 'your pet' },
+	owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+	type: { type: String },
+	age: { type: Number, required: true, default: 0 }, // age in days
+	happiness_level: { type: Number, default: 50, max: 100 },
+	hunger_level: { type: Number, default: 50, max: 100 },
+	cleanliness: { type: Number, default: 50, max: 100 },
 });
 
-const Pet = mongoose.model("Pet", PetSchema);
+const Pet = mongoose.model('Pet', PetSchema);
 
 export default Pet;
--- src/models/user.mjs
+++ src/models/user.mjs
@@ -3,7 +3,7 @@ import passportLocalMongoose from 'passport-local-mongoose';
 
 const UserSchema = new mongoose.Schema({
 	username: { type: String, unique: true, required: true },
-	password: { type: String},
+	password: { type: String },
 	pet: { type: mongoose.Schema.Types.ObjectId, ref: 'Pet' },
 });
 
